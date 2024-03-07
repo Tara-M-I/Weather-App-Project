9be91ea5f9e43dbf27c815959281da9e55a2a865
Zaydra D <161665673+Tara-M-I@users.noreply.github.com>
2024-03-07 12:07:04 -0500

    fixed bugs in apiUrl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function updateWeatherInfo(response){
+console.log(response.data.temperature.current);  
+}
+
+function searchTemp(city){
+let apiKey = "c794c14abfa5138e0adb4dof9fffect7";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeatherInfo);                                                                                            
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchCity = document.querySelector("#search-city");
    let cityElement = document.querySelector("#city");
    cityElement.innerHTML = searchCity.value;
+   searchTemp(searchCity.value);
 }
 
 let searchFormElement = document.querySelector("#search-form-input");
